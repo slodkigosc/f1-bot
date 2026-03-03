6e826f30d32c8d3443489d323d619e85cab01063
slodkigosc <146870679+slodkigosc@users.noreply.github.com>
2026-03-03 11:04:28 +0100

    database.js

--- database.js
+++ database.js
@@ -1,12 +1,1 @@
-const Database = require("better-sqlite3");
 
-const db = new Database("database.db");
-
-db.prepare(`
-CREATE TABLE IF NOT EXISTS jokers (
-  user_id TEXT PRIMARY KEY,
-  count INTEGER DEFAULT 2
-)
-`).run();
-
-module.exports = db;
